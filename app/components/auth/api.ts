37158eafba4fd37ee1a54001a5fe57e93cddb521
Nikhil Rajpoot <niksocial2@gmail.com>
2026-03-12 11:23:50 +0530

    Change json type from any | null to any

--- app/components/auth/api.ts
+++ app/components/auth/api.ts
@@ -21,7 +21,7 @@ export async function postJSON(path: string, body: any) {
     },
   );
 
-  let json: any | null = null;
+  let json: any = null;
   try {
     json = await res.json();
   } catch (e) {
